8ea749c2157d9c37a5a4913d8d24fecefa2decbc
真心 <ceo@iqianye.cn>
2022-09-24 10:35:04 +0800

    api: 更正身份组成员API注释

--- qqbot-api/src/main/java/me/zhenxin/qqbot/api/impl/RoleApi.java
+++ qqbot-api/src/main/java/me/zhenxin/qqbot/api/impl/RoleApi.java
@@ -92,7 +92,7 @@ public class RoleApi extends BaseApi {
     }
 
     /**
-     * 增加身份组成员 (仅私域可用)
+     * 增加身份组成员
      *
      * @param guildId   频道ID
      * @param userId    用户ID
@@ -110,7 +110,7 @@ public class RoleApi extends BaseApi {
     }
 
     /**
-     * 删除身份组成员 (仅私域可用)
+     * 删除身份组成员
      *
      * @param guildId   频道ID
      * @param userId    用户ID
