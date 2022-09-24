fa77af2661aa8d9eab8fdd4c23f816ae75d18f42
真心 <ceo@iqianye.cn>
2022-09-24 10:34:12 +0800

    api: 补充成员API

--- qqbot-api/src/main/java/me/zhenxin/qqbot/api/impl/MemberApi.java
+++ qqbot-api/src/main/java/me/zhenxin/qqbot/api/impl/MemberApi.java
@@ -38,7 +38,7 @@ public class MemberApi extends BaseApi {
     }
 
     /**
-     * 获取成员列表
+     * 获取成员列表 (仅私域可用)
      *
      * @param guildId 频道ID
      * @param limit   获取数量
@@ -49,7 +49,7 @@ public class MemberApi extends BaseApi {
     }
 
     /**
-     * 获取成员列表
+     * 获取成员列表 (仅私域可用)
      *
      * @param guildId 频道ID
      * @param after   频道ID (读此ID之后的数据)
@@ -58,6 +58,34 @@ public class MemberApi extends BaseApi {
     public List<Member> getMemberList(String guildId, String after, Integer limit) throws ApiException {
         StringBuilder sb = new StringBuilder("/guilds/" + guildId + "/members");
         sb.append("?after=").append(after);
+        sb.append("&limit=").append(limit);
+        JSONArray array = get(sb.toString(), JSONArray.class);
+        return array.toJavaList(Member.class);
+    }
+
+    /**
+     * 获取身份组成员列表 (仅私域可用)
+     *
+     * @param guildId 频道ID
+     * @param roleId  身份组ID
+     * @param limit   获取数量
+     */
+    public List<Member> getMemberListByRole(String guildId, String roleId, Integer limit) throws ApiException {
+        JSONArray array = get("/guilds/" + guildId + "/roles/" + roleId + "/members?limit=" + limit, JSONArray.class);
+        return array.toJavaList(Member.class);
+    }
+
+    /**
+     * 获取身份组成员列表 (仅私域可用)
+     *
+     * @param guildId    频道ID
+     * @param roleId     身份组ID
+     * @param startIndex 将上一次回包中next填入， 如果是第一次请求填 0，默认为 0
+     * @param limit      获取数量
+     */
+    public List<Member> getMemberListByRole(String guildId, String roleId, String startIndex, Integer limit) throws ApiException {
+        StringBuilder sb = new StringBuilder("/guilds/" + guildId + "/roles/" + roleId + "/members");
+        sb.append("?start_index=").append(startIndex);
         sb.append("&limit=").append(limit);
         JSONArray array = get(sb.toString(), JSONArray.class);
         return array.toJavaList(Member.class);
